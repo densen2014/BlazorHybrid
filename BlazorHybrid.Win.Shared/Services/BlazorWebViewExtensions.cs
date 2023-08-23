52222668ff710d779900a8ed179f10e6c4f9a703
Alex chow <zhouchuanglin@gmail.com>
2023-08-24 01:17:25 +0800

    Create BlazorWebViewExtensions.cs

---
+++ BlazorHybrid.Win.Shared/Services/BlazorWebViewExtensions.cs
@@ -0,0 +1,14 @@
+﻿#if WEBVIEW2_WINFORMS
+using Microsoft.Web.WebView2.Core;
+using WebView2Control = Microsoft.Web.WebView2.WinForms.WebView2;
+#elif WEBVIEW2_WPF
+using Microsoft.Web.WebView2.Core;
+using WebView2Control = Microsoft.Web.WebView2.Wpf.WebView2;
+#endif
+
+namespace BlazorHybrid.Maui.Shared;
+
+public partial class InitBlazorWebView 
+{
+    
+}
