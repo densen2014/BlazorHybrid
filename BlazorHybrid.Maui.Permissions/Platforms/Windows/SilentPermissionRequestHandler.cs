8bdd2e47edda5b89747de7a4da8554f52ef85e03
Alex chow <zhouchuanglin@gmail.com>
2023-08-26 03:28:30 +0800

    Update SilentPermissionRequestHandler.cs

--- BlazorHybrid.Maui.Permissions/Platforms/Windows/SilentPermissionRequestHandler.cs
+++ BlazorHybrid.Maui.Permissions/Platforms/Windows/SilentPermissionRequestHandler.cs
@@ -11,10 +11,12 @@ namespace BlazorMaui.Platforms.Windows;
 internal class SilentPermissionRequestHandler : IPermissionRequestHandler
 {
     private static readonly Uri BaseUri = new("https://0.0.0.0");
+    private static readonly Uri BaseUri2 = new("https://blazor.app1.es/");
 
     public void OnPermissionRequested(CoreWebView2 sender, CoreWebView2PermissionRequestedEventArgs args)
     {
-        args.State = Uri.TryCreate(args.Uri, UriKind.RelativeOrAbsolute, out var uri) && BaseUri.IsBaseOf(uri) ?
+        args.State = Uri.TryCreate(args.Uri, UriKind.RelativeOrAbsolute, out var uri) && (BaseUri.IsBaseOf(uri)
+            || BaseUri2.IsBaseOf(uri)) ?
             CoreWebView2PermissionState.Allow :
             CoreWebView2PermissionState.Deny;
         args.Handled = true;
