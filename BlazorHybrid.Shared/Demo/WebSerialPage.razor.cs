d10c8591aa903b3030045217de3543628134c688
Alex Chow <zhouchuanglin@gmail.com>
2023-09-12 20:32:30 +0200

    WebSerialPage

--- BlazorHybrid.Shared/Demo/WebSerialPage.razor.cs
+++ BlazorHybrid.Shared/Demo/WebSerialPage.razor.cs
@@ -6,8 +6,11 @@
 
 
 using BootstrapBlazor.Components;
+using System;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
+using System.Net.NetworkInformation;
+using System.Threading;
 
 namespace BlazorHybrid.Shared.Pages;
 
@@ -17,21 +20,47 @@ public partial class WebSerialPage
     private string? message;
     private string? statusmessage;
     private string? errmessage;
-    private WebSerialOptions options = new WebSerialOptions() { BaudRate = 115200 };
+    private WebSerialOptions options = new WebSerialOptions() { BaudRate = 115200, AutoGetSignals = true };
 
     [NotNull]
     private IEnumerable<SelectedItem> BaudRateList { get; set; } = ListToSelectedItem();
 
     [DisplayName("波特率")]
     private int SelectedBaudRate { get; set; } = 115200;
-    private bool Flag { get; set; } 
-    private bool IsConnected { get; set; } 
-    private WebSerial? WebSerial { get; set; } 
+    private bool Flag { get; set; }
+    private bool IsConnected { get; set; }
+    private WebSerial? WebSerial { get; set; }
 
- 
+    /// <summary>
+    /// 收到的信号数据
+    /// </summary>
+    public WebSerialSignals Signals { get; set; } = new WebSerialSignals();
+
+
     private Task OnReceive(string? message)
     {
-        this.message = $"{DateTime.Now:hh:mm:ss} 收到数据: {message}{Environment.NewLine}"+ this.message;
+        this.message = $"{DateTime.Now:hh:mm:ss} 收到数据: {message}{Environment.NewLine}" + this.message;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+    private Task OnSignals(WebSerialSignals? signals)
+    {
+        if (signals is null) return Task.CompletedTask;
+
+        this.Signals = signals;
+
+        if (!options.AutoGetSignals)
+        {
+            // 仅在不自动获取信号时才显示
+            this.message = $"{DateTime.Now:hh:mm:ss} 收到信号数据: {Environment.NewLine}" +
+                            $"RING:  {signals.RING}{Environment.NewLine}" +
+                            $"DSR:   {signals.DSR}{Environment.NewLine}" +
+                            $"CTS:   {signals.CTS}{Environment.NewLine}" +
+                            $"DCD:   {signals.DCD}{Environment.NewLine}" +
+                            $"{message}{Environment.NewLine}";
+        }
+
         StateHasChanged();
         return Task.CompletedTask;
     }
@@ -39,14 +68,19 @@ public partial class WebSerialPage
     private Task OnConnect(bool flag)
     {
         this.IsConnected = flag;
-        if (flag) {
+        if (flag)
+        {
             message = null;
             statusmessage = null;
             errmessage = null;
-        }
-        else
-        {
-            //Flag=false;
+            Task.Run(async () =>
+            {
+                while (WebSerial == null)
+                {
+                    await Task.Delay(300);
+                }
+                await InvokeAsync(StateHasChanged);
+            });
         }
         StateHasChanged();
         return Task.CompletedTask;
@@ -73,6 +107,7 @@ public partial class WebSerialPage
             yield return new SelectedItem(item.ToString(), item.ToString());
         }
     }
+
     private Task OnApply()
     {
         options.BaudRate = SelectedBaudRate;
@@ -80,6 +115,5 @@ public partial class WebSerialPage
         //StateHasChanged();
         return Task.CompletedTask;
     }
-
-     
+ 
 }
