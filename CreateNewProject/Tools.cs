58282932a3544fcb7071758281fbce7a06efd210
Alex chow <zhouchuanglin@gmail.com>
2023-10-14 03:10:42 +0200

    Update Tools.cs

--- CreateNewProject/Tools.cs
+++ CreateNewProject/Tools.cs
@@ -45,7 +45,7 @@ public class Tools
             }
             var targetFilePath = Path.Combine(destinationDir, filename);
             file.CopyTo(targetFilePath);
-            if (new[] { ".csproj", ".cshtml", ".html", ".cs", ".razor", ".xaml", ".md" }.Contains(extName))
+            if (new[] { ".csproj", ".cshtml", ".html", ".cs", ".razor", ".xaml", ".md", ".xml", ".manifest", ".json" }.Contains(extName))
             {
                 var content = File.ReadAllText(targetFilePath);
                 var new_htmcontent = content.Replace("BlazorHybrid", projectName);
