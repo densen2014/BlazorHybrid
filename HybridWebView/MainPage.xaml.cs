2f87a64fbb8bb7f7e3576424db69bcc9c67504f1
Alex chow <zhouchuanglin@gmail.com>
2024-07-10 13:16:42 +0200

    添加 HybridWebView - WebViewNativeApi 套壳跨平台执行本机c#的api

---
+++ HybridWebView/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using System.Text;
+using WebViewNativeApi;
+
+namespace HybridWebView
+{
+    public partial class MainPage : ContentPage
+    {
+        private int count = 0;
+        private NativeBridge? api;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            WebView? wvBrowser = FindByName("webView") as WebView;
+            api = new NativeBridge(wvBrowser);
+            api.AddTarget("dialogs", new NativeApi());
+
+        }
+
+    }
+
+    internal class NativeApi : object
+    {
+        public async Task<string> open_file_dialog()
+        {
+            //work in ui thread
+            var res =
+            await MainThread.InvokeOnMainThreadAsync(async () =>
+            {
+                try
+                {
+                    var result = await FilePicker.Default.PickAsync(new PickOptions());
+                    if (result == null)
+                    {
+                        return "";
+                    }
+                    using var stream = await result.OpenReadAsync();
+                    StreamReader reader = new StreamReader(stream);
+                    return Convert.ToBase64String(Encoding.UTF8.GetBytes(reader.ReadToEnd()));
+                }
+                catch (Exception e)
+                {
+                    var err = e.Message;
+                    return err;
+                }
+            });
+            return res;
+        }
+
+        public async Task<string> save_file(string data, string fileName)
+        {
+            try
+            {
+                string targetFile = System.IO.Path.Combine(FileSystem.Current.AppDataDirectory, fileName);
+
+                using FileStream outputStream = File.OpenWrite(targetFile);
+                using StreamWriter streamWriter = new(outputStream);
+
+                await streamWriter.WriteAsync(data);
+            }
+            catch (Exception e)
+            {
+                var err = e.Message;
+                return err;
+            }
+            return "ok";
+        }
+    }
+}
