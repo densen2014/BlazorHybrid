ba72b6ae239e22862ca7fbf712788e6690297fcf
Alex chow <zhouchuanglin@gmail.com>
2024-07-10 11:42:39 +0200

    Update MainPage.xaml.cs

--- HybridWebView/MainPage.xaml.cs
+++ HybridWebView/MainPage.xaml.cs
@@ -12,7 +12,7 @@ namespace HybridWebView
     public partial class MainPage : ContentPage
     {
         int count = 0;
-        private NativeBridge? api; 
+        private NativeBridge? api;
 
         public MainPage()
         {
@@ -21,50 +21,56 @@ namespace HybridWebView
             api = new NativeBridge(wvBrowser);
             api.AddTarget("dialogs", new NativeApi());
 
-        } 
+        }
 
     }
-    
+
     class NativeApi : object
     {
         public async Task<string> open_file_dialog()
         {
             //work in ui thread
-            var res=
-            await MainThread.InvokeOnMainThreadAsync(async() =>
+            var res =
+            await MainThread.InvokeOnMainThreadAsync(async () =>
             {
-                // Code to run on the main thread
+                try
+                {
+                    var result = await FilePicker.Default.PickAsync(new PickOptions());
+                    if (result == null)
+                    {
+                        return "";
+                    }
+                    using var stream = await result.OpenReadAsync();
+                    StreamReader reader = new StreamReader(stream);
+                    return Convert.ToBase64String(Encoding.UTF8.GetBytes(reader.ReadToEnd()));
 
-
-            try
-            {
-            var result = await FilePicker.Default.PickAsync(new PickOptions());
-            if (result == null)
-            {
-                return "";
-            }
-            using var stream = await result.OpenReadAsync();
-            StreamReader reader = new StreamReader(stream);
-            return Convert.ToBase64String(Encoding.UTF8.GetBytes(reader.ReadToEnd()));
-
-            }
-            catch (Exception e)
-            {
-                var err=e.Message ;
-                return err;
+                }
+                catch (Exception e)
+                {
+                    var err = e.Message;
+                    return err;
                 }
             });
             return res;
         }
 
-        public async void save_file(string data, string fileName)
+        public async Task<string> save_file(string data, string fileName)
         {
-            string targetFile = System.IO.Path.Combine(FileSystem.Current.AppDataDirectory, fileName);
+            try
+            {
+                string targetFile = System.IO.Path.Combine(FileSystem.Current.AppDataDirectory, fileName);
 
-            using FileStream outputStream = File.OpenWrite(targetFile);
-            using StreamWriter streamWriter = new(outputStream);
+                using FileStream outputStream = File.OpenWrite(targetFile);
+                using StreamWriter streamWriter = new(outputStream);
 
-            await streamWriter.WriteAsync(data);
+                await streamWriter.WriteAsync(data);
+            }
+            catch (Exception e)
+            {
+                var err = e.Message;
+                return err;
+            }
+            return "ok";
         }
     }
 }
