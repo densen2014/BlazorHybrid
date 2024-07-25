0104d6fe5144b0a354dfb3f7f23169f30abe1940
Alex chow <zhouchuanglin@gmail.com>
2024-07-25 10:40:59 +0200

    +

---
+++ test1/BtPrinter.xaml.cs
@@ -0,0 +1,28 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+namespace HybridWebView;
+
+public partial class BtPrinter : ContentView
+{
+    private string PrinterNameKey = "PrinterName";
+    private string printerName = "Unknown";
+
+    public BtPrinter()
+    {
+        InitializeComponent();
+        PrinterName.Text = printerName = Preferences.Default.Get(PrinterNameKey, printerName);
+    }
+
+    private void OnTestClicked(object sender, EventArgs e)
+    {
+        printerName = $"Printer {DateTime.Now.Microsecond}";
+        Preferences.Default.Set(PrinterNameKey, printerName);
+        PrinterName.Text = printerName;
+    }
+
+
+}
---
+++ test1/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace test1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
+
