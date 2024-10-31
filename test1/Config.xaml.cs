a76f5f2c1d963792e5ba5cbaa0d97a14b7739d23
Alex chow <zhouchuanglin@gmail.com>
2024-10-31 11:40:34 +0100

    +

---
+++ test1/Config.xaml.cs
@@ -0,0 +1,17 @@
+﻿// **********************************
+// Densen Informatica 中讯科技
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com
+// **********************************
+
+namespace test1;
+
+public partial class Config : ContentView
+{
+
+    public Config()
+    {
+        InitializeComponent(); 
+    }
+
+}
