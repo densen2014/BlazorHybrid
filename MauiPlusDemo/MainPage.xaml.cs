bf11f513b85c6044c3dd6c0348ea09479e81c409
Alex chow <zhouchuanglin@gmail.com>
2025-02-11 16:10:09 +0100

    添加 MauiPlus 库演示 MauiPlusDemo 工程

---
+++ MauiPlusDemo/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using MauiPlus;
+
+namespace MauiPlusDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        private NativeBridge? api;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //附加本机功能处理
+            WebView? wvBrowser = FindByName("webView") as WebView;
+            if (wvBrowser != null)
+            {
+                LoadHtmlToWebView(wvBrowser);
+                api = new NativeBridge(wvBrowser);
+                api.AddTarget("dialogs", new NativeApi());
+            }
+
+#if MACCATALYST
+    Microsoft.Maui.Handlers.WebViewHandler.Mapper.AppendToMapping("Inspect", (handler, view) =>
+    {
+        if (OperatingSystem.IsMacCatalystVersionAtLeast(16, 6))
+            handler.PlatformView.Inspectable = true;
+    });
+#endif
+        }
+
+        private async void LoadHtmlToWebView(WebView wvBrowser)
+        {
+            // 加载本地 HTML 文件
+            var htmlSource = new HtmlWebViewSource
+            {
+                BaseUrl = FileSystem.AppDataDirectory,
+                Html = await LoadLocalHtml("demo.html")
+            };
+            wvBrowser.Source = htmlSource;
+        }
+
+        private async Task<string> LoadLocalHtml(string fileName)
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync(fileName);
+            using var reader = new StreamReader(stream);
+
+            var contents = await reader.ReadToEndAsync();
+            return contents;
+        }
+    }
+
+}
---
+++ MauiPlusDemo/NativeApi.cs
@@ -0,0 +1,121 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using System.Text;
+using System.Text.Json;
+
+namespace MauiPlusDemo;
+
+internal partial class NativeApi : object
+{
+    private string PrinterNameKey = "PrinterName";
+    private string printerName = "Unknown";
+
+    public Task<string> set_config(string printerName)
+    {
+        Preferences.Set(PrinterNameKey, printerName);
+        return Task.FromResult("ok");
+    }
+
+    /// <summary>
+    /// 从应用程序的首选项中获取打印机名称 (printerName)
+    /// </summary>
+    /// <returns></returns>
+    public Task<string> get_config()
+    {
+        printerName = Preferences.Default.Get(PrinterNameKey, printerName);
+        return Task.FromResult(printerName);
+    }
+
+    /// <summary>
+    /// 打开文件选择对话框,读取文件内容并将其转换为 Base64 编码的字符串返回
+    /// </summary>
+    /// <returns>文件内容 Base64 编码的字符串</returns>
+    public async Task<string> open_file_dialog()
+    {
+        //work in ui thread
+        var res =
+        await MainThread.InvokeOnMainThreadAsync(async () =>
+        {
+            try
+            {
+                var result = await FilePicker.Default.PickAsync(new PickOptions());
+                if (result == null)
+                {
+                    return "";
+                }
+                using var stream = await result.OpenReadAsync();
+                StreamReader reader = new StreamReader(stream);
+                return Convert.ToBase64String(Encoding.UTF8.GetBytes(reader.ReadToEnd()));
+            }
+            catch (Exception e)
+            {
+                var err = e.Message;
+                return err;
+            }
+        });
+        return res;
+    }
+
+    /// <summary>
+    /// 将给定的数据保存到指定文件名的文件,返回文件路径
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="fileName"></param>
+    /// <returns>文件路径</returns>
+    public async Task<string> save_file(string data, string fileName)
+    {
+        try
+        {
+            string targetFile = System.IO.Path.Combine(FileSystem.Current.AppDataDirectory, fileName);
+
+            using FileStream outputStream = File.OpenWrite(targetFile);
+            using StreamWriter streamWriter = new(outputStream);
+
+            await streamWriter.WriteAsync(data);
+            return $"file path:{targetFile.Replace("\\", "\\\\")}";
+        }
+        catch (Exception e)
+        {
+            var err = e.Message;
+            return err;
+        }
+    }
+
+    /// <summary>
+    /// 存储自定义对象 User, 将自定义对象序列化为 string 类型，然后再存储
+    /// </summary>
+    /// <param name="user"></param>
+    public void SaveUser(User user)
+    {
+        string jsonString = JsonSerializer.Serialize(user);
+        Preferences.Set("user", jsonString);
+    }
+
+    /// <summary>
+    /// 检索自定义对象 User, 从 Preferences 中检索字符串，然后将其反序列化为自定义对象
+    /// </summary>
+    /// <returns></returns>
+    public User? GetUser()
+    {
+        string jsonString = Preferences.Get("user", string.Empty);
+        if (string.IsNullOrEmpty(jsonString))
+        {
+            return null;
+        }
+        return JsonSerializer.Deserialize<User>(jsonString);
+    }
+
+}
+
+/// <summary>
+/// 在 Preferences 中存储自定义对象, https://www.cnblogs.com/densen2014/p/18710319
+/// </summary>
+public class User
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
